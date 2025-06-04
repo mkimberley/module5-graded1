4fe517b09286f3ac803a9876634ef195d9eaca03
Matt Kimberley <matt.kimberley@hotmail.com>
2025-06-04 09:29:39 +0100

    Updated run script to ensure a virtual env is used

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-source ~/venv/bin/activate
+# Check if the virtual environment exists
+if [ ! -d "$HOME/venv_squeezy" ]; then
+    echo "Virtual environment not found. Creating a new one..."
+    python3 -m venv "$HOME/venv_squeezy"
+else
+    echo "Virtual environment found."
+fi
+source "$HOME/venv/bin/activate"
+
+pip install -r src/requirements.txt
 
 # Run pytest and check its exit status
+echo "Running tests..."
 pytest
 if [ $? -eq 0 ]; then
+    echo "Tests passed. Proceeding to run the application."
     cd src
-    pip install -r requirements.txt
     python3 main.py
 else
     echo "Tests failed. Aborting further execution."
